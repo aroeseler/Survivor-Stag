1935a25b937a3cbf748daeedf0843c568c8c7cfc
hmc-catfish-f18 <aroeseler@g.hmc.edu>
2019-04-26 11:29:25 -0700

    failed to implement ammo pickups

---
+++ Assets/Scripts/PlayerScripts/ItemPickup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public Collider body;
+    public Collider player;
+
+    void Start()
+    {
+        if (body == null)
+        {
+            body = GetComponent<Collider>();
+        }
+    }
+
+    void Update()
+    {
+        if (body.bounds.Intersects(player.bounds))
+        {
+            player.SendMessage("updateAmmo", +50);
+        }
+    }
+}
